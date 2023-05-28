8e60b3610f3f236d8a3a033aeaa6982719e3fbcf
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-05-28 16:44:50 +0900

    mainを修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'bulma/css/bulma.css';
+
+import App from '@/App.tsx';
+import '@/index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
