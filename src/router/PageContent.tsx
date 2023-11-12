835c198d57051162ccef92c6950a60bbc5042e58
Shumpei Yamamoto <shumpeta.kus@gmail.com>
2023-11-12 18:36:41 +0900

    FontAwesomeを追加

--- src/router/PageContent.tsx
+++ src/router/PageContent.tsx
@@ -1,14 +1,12 @@
 import { MemoList } from "@/page/MemoList";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 
 const PageContent = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/memos" element={<MemoList />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/memos" element={<MemoList />} />
+    </Routes>
   )
 };
 
